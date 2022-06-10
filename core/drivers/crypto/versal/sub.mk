5a5b212ff3fdea398887454ddf79fff2640ec3a5
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-06-10 19:34:06 +0200

    optee: fix crypto sub.mk
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- core/drivers/crypto/versal/sub.mk
+++ core/drivers/crypto/versal/sub.mk
@@ -4,7 +4,7 @@ cflags-y += -Wno-unused-parameter
 
 srcs-y += ipi.c
 srcs-y += ecc.c
-srcs-y += rsa.c rsa_test.c
+srcs-y += rsa.c
 srcs-y += hash.c
 srcs-y += authenc.c
 srcs-$(CFG_VERSAL_TESTS) += ecc_test.c rsa_test.c hash_test.c authenc_test.c
