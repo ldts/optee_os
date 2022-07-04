e97569dce6cf895eb958bf89aa34084363e79c8b
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-07-04 14:11:42 +0200

    libtomcrypt/mbedtls: rsa: prefix the interface with sw_
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- lib/libmbedtls/core/rsa.c
+++ lib/libmbedtls/core/rsa.c
@@ -128,6 +128,10 @@ static void mbd_rsa_free(mbedtls_rsa_context *rsa)
 
 TEE_Result crypto_acipher_alloc_rsa_keypair(struct rsa_keypair *s,
 					    size_t key_size_bits)
+__attribute__((weak, alias("sw_crypto_acipher_alloc_rsa_keypair")));
+
+TEE_Result sw_crypto_acipher_alloc_rsa_keypair(struct rsa_keypair *s,
+					       size_t key_size_bits)
 {
 	memset(s, 0, sizeof(*s));
 	s->e = crypto_bignum_allocate(key_size_bits);
@@ -163,6 +167,10 @@ err:
 
 TEE_Result crypto_acipher_alloc_rsa_public_key(struct rsa_public_key *s,
 					       size_t key_size_bits)
+__attribute__((weak, alias("sw_crypto_acipher_alloc_rsa_public_key")));
+
+TEE_Result sw_crypto_acipher_alloc_rsa_public_key(struct rsa_public_key *s,
+						  size_t key_size_bits)
 {
 	memset(s, 0, sizeof(*s));
 	s->e = crypto_bignum_allocate(key_size_bits);
@@ -178,6 +186,9 @@ err:
 }
 
 void crypto_acipher_free_rsa_public_key(struct rsa_public_key *s)
+__attribute__((weak, alias("sw_crypto_acipher_free_rsa_public_key")));
+
+void sw_crypto_acipher_free_rsa_public_key(struct rsa_public_key *s)
 {
 	if (!s)
 		return;
@@ -186,6 +197,9 @@ void crypto_acipher_free_rsa_public_key(struct rsa_public_key *s)
 }
 
 void crypto_acipher_free_rsa_keypair(struct rsa_keypair *s)
+__attribute__((weak, alias("sw_crypto_acipher_free_rsa_keypair")));
+
+void sw_crypto_acipher_free_rsa_keypair(struct rsa_keypair *s)
 {
 	if (!s)
 		return;
@@ -199,7 +213,12 @@ void crypto_acipher_free_rsa_keypair(struct rsa_keypair *s)
 	crypto_bignum_free(s->dq);
 }
 
-TEE_Result crypto_acipher_gen_rsa_key(struct rsa_keypair *key, size_t key_size)
+TEE_Result crypto_acipher_gen_rsa_key(struct rsa_keypair *key,
+				      size_t key_size)
+__attribute__((weak, alias("sw_crypto_acipher_gen_rsa_key")));
+
+TEE_Result sw_crypto_acipher_gen_rsa_key(struct rsa_keypair *key,
+					 size_t key_size)
 {
 	TEE_Result res = TEE_SUCCESS;
 	mbedtls_rsa_context rsa;
@@ -240,8 +259,15 @@ TEE_Result crypto_acipher_gen_rsa_key(struct rsa_keypair *key, size_t key_size)
 }
 
 TEE_Result crypto_acipher_rsanopad_encrypt(struct rsa_public_key *key,
-					   const uint8_t *src, size_t src_len,
-					   uint8_t *dst, size_t *dst_len)
+					   const uint8_t *src,
+					   size_t src_len, uint8_t *dst,
+					   size_t *dst_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsanopad_encrypt")));
+
+TEE_Result sw_crypto_acipher_rsanopad_encrypt(struct rsa_public_key *key,
+					      const uint8_t *src,
+					      size_t src_len, uint8_t *dst,
+					      size_t *dst_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	mbedtls_rsa_context rsa;
@@ -298,8 +324,15 @@ out:
 }
 
 TEE_Result crypto_acipher_rsanopad_decrypt(struct rsa_keypair *key,
-					   const uint8_t *src, size_t src_len,
-					   uint8_t *dst, size_t *dst_len)
+					   const uint8_t *src,
+					   size_t src_len, uint8_t *dst,
+					   size_t *dst_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsanopad_decrypt")));
+
+TEE_Result sw_crypto_acipher_rsanopad_decrypt(struct rsa_keypair *key,
+					      const uint8_t *src,
+					      size_t src_len, uint8_t *dst,
+					      size_t *dst_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	mbedtls_rsa_context rsa;
@@ -347,11 +380,20 @@ out:
 	return res;
 }
 
-TEE_Result crypto_acipher_rsaes_decrypt(uint32_t algo, struct rsa_keypair *key,
+TEE_Result crypto_acipher_rsaes_decrypt(uint32_t algo,
+					struct rsa_keypair *key,
 					const uint8_t *label __unused,
 					size_t label_len __unused,
 					const uint8_t *src, size_t src_len,
 					uint8_t *dst, size_t *dst_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsaes_decrypt")));
+
+TEE_Result sw_crypto_acipher_rsaes_decrypt(uint32_t algo,
+					   struct rsa_keypair *key,
+					   const uint8_t *label __unused,
+					   size_t label_len __unused,
+					   const uint8_t *src, size_t src_len,
+					   uint8_t *dst, size_t *dst_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	int lmd_res = 0;
@@ -441,6 +483,14 @@ TEE_Result crypto_acipher_rsaes_encrypt(uint32_t algo,
 					size_t label_len __unused,
 					const uint8_t *src, size_t src_len,
 					uint8_t *dst, size_t *dst_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsaes_encrypt")));
+
+TEE_Result sw_crypto_acipher_rsaes_encrypt(uint32_t algo,
+					   struct rsa_public_key *key,
+					   const uint8_t *label __unused,
+					   size_t label_len __unused,
+					   const uint8_t *src, size_t src_len,
+					   uint8_t *dst, size_t *dst_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	int lmd_res = 0;
@@ -507,9 +557,15 @@ out:
 }
 
 TEE_Result crypto_acipher_rsassa_sign(uint32_t algo, struct rsa_keypair *key,
-				      int salt_len __unused, const uint8_t *msg,
-				      size_t msg_len, uint8_t *sig,
-				      size_t *sig_len)
+				      int salt_len __unused,
+				      const uint8_t *msg, size_t msg_len,
+				      uint8_t *sig, size_t *sig_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsassa_sign")));
+
+TEE_Result sw_crypto_acipher_rsassa_sign(uint32_t algo, struct rsa_keypair *key,
+					 int salt_len __unused,
+					 const uint8_t *msg, size_t msg_len,
+					 uint8_t *sig, size_t *sig_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	int lmd_res = 0;
@@ -599,6 +655,14 @@ TEE_Result crypto_acipher_rsassa_verify(uint32_t algo,
 					const uint8_t *msg,
 					size_t msg_len, const uint8_t *sig,
 					size_t sig_len)
+__attribute__((weak, alias("sw_crypto_acipher_rsassa_verify")));
+
+TEE_Result sw_crypto_acipher_rsassa_verify(uint32_t algo,
+					   struct rsa_public_key *key,
+					   int salt_len __unused,
+					   const uint8_t *msg,
+					   size_t msg_len, const uint8_t *sig,
+					   size_t sig_len)
 {
 	TEE_Result res = TEE_SUCCESS;
 	int lmd_res = 0;
