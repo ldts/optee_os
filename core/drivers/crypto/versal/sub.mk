7affcb225f91fee039d1457f2a79b9fcd3484369
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-07-04 17:07:56 +0200

    versal: local tests
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- core/drivers/crypto/versal/sub.mk
+++ core/drivers/crypto/versal/sub.mk
@@ -6,3 +6,5 @@ srcs-y += hash.c
 srcs-y += ecc.c
 srcs-y += rsa.c
 
+srcs-$(CFG_VERSAL_TESTS) += ecc_test.c rsa_test.c hash_test.c authenc_test.c
+
--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -62,6 +62,7 @@ srcs-$(CFG_VERSAL_PM) += versal_pm.c
 srcs-$(CFG_VERSAL_NVM) += versal_nvm.c
 srcs-$(CFG_VERSAL_PUF) += versal_puf.c
 srcs-$(CFG_VERSAL_HUK) += versal_huk.c
+srcs-$(CFG_VERSAL_TESTS) += versal_puf_test.c versal_nvm_test.c versal_gpio_test.c
 
 subdirs-y += crypto
 subdirs-$(CFG_BNXT_FW) += bnxt
