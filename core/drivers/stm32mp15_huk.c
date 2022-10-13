c4c8a62440f485c2215d1f58882b7c540e9374fa
Jorge Ramirez-Ortiz <jorge@foundries.io>
2022-10-13 22:16:49 +0200

    [FIO internal] update to latest HUK version
    Update to latest HUK (closer to final version)
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- core/drivers/stm32mp15_huk.c
+++ core/drivers/stm32mp15_huk.c
@@ -12,6 +12,7 @@
 #include <kernel/tee_common_otp.h>
 #include <mempool.h>
 #include <platform_config.h>
+#include <stm32_util.h>
 #include <string.h>
 #include <string_ext.h>
 
@@ -22,13 +23,20 @@ static TEE_Result stm32mp15_read_uid(uint32_t *uid)
 {
 	uint32_t *q = uid;
 
-	if (stm32_bsec_shadow_read_otp(q++, UID0_OTP))
+	/*
+	 * Shadow memory for UID words might not be locked: to guarante that
+	 * the final values are read we must lock them.
+	 */
+	if (stm32_bsec_set_sw_lock(UID0_OTP) ||
+	    stm32_bsec_shadow_read_otp(q++, UID0_OTP))
 		return TEE_ERROR_GENERIC;
 
-	if (stm32_bsec_shadow_read_otp(q++, UID1_OTP))
+	if (stm32_bsec_set_sw_lock(UID1_OTP) ||
+	    stm32_bsec_shadow_read_otp(q++, UID1_OTP))
 		return TEE_ERROR_GENERIC;
 
-	if (stm32_bsec_shadow_read_otp(q++, UID2_OTP))
+	if (stm32_bsec_set_sw_lock(UID2_OTP) ||
+	    stm32_bsec_shadow_read_otp(q++, UID2_OTP))
 		return TEE_ERROR_GENERIC;
 
 	return TEE_SUCCESS;
@@ -36,13 +44,19 @@ static TEE_Result stm32mp15_read_uid(uint32_t *uid)
 
 static TEE_Result stm32mp15_read_otp(uint32_t otp, uint32_t *key, bool *locked)
 {
-	bool tmp = false;
+	bool tmp = true;
 
-	if (stm32_bsec_read_permanent_lock(otp, &tmp))
-		return TEE_ERROR_GENERIC;
+	if (!stm32mp_is_closed_device()) {
+		/*
+		 * When the device is not closed, the shadow memory for these
+		 * words might not be locked: check and report them
+		 */
+		if (stm32_bsec_read_permanent_lock(otp, &tmp))
+			return TEE_ERROR_GENERIC;
 
-	if (tmp && stm32_bsec_read_sw_lock(otp, &tmp))
-		return TEE_ERROR_GENERIC;
+		if (tmp && stm32_bsec_read_sw_lock(otp, &tmp))
+			return TEE_ERROR_GENERIC;
+	}
 
 	if (stm32_bsec_shadow_read_otp(key, otp))
 		return TEE_ERROR_GENERIC;
@@ -92,8 +106,6 @@ static TEE_Result aes_gcm_encrypt_uid(uint8_t *key, size_t key_len,
 auth_err:
 	crypto_authenc_free_ctx(ctx);
 err:
-	memzero_explicit(uid, uid_len);
-	memzero_explicit(tag, tag_len);
 	if (ret)
 		memzero_explicit(out, *out_len);
 
@@ -102,22 +114,16 @@ err:
 
 TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 {
-	size_t len = HW_UNIQUE_KEY_LENGTH;
+	uint32_t otp_key[4] = { 0 };
+	size_t len = sizeof(otp_key);
 	TEE_Result ret = TEE_SUCCESS;
-	uint32_t *key = NULL;
-	uint8_t *p = NULL;
-	bool lock = false;
+	uint32_t *key = otp_key;
+	bool lock = true;
 
 	static_assert(CFG_STM32MP15_HUK_BSEC_KEY_0 < STM32MP1_OTP_MAX_ID);
 	static_assert(CFG_STM32MP15_HUK_BSEC_KEY_1 < STM32MP1_OTP_MAX_ID);
 	static_assert(CFG_STM32MP15_HUK_BSEC_KEY_2 < STM32MP1_OTP_MAX_ID);
 	static_assert(CFG_STM32MP15_HUK_BSEC_KEY_3 < STM32MP1_OTP_MAX_ID);
-
-	key = mempool_calloc(mempool_default, 1, len);
-	if (!key)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	p = (uint8_t *)key;
 
 	ret = stm32mp15_read_otp(CFG_STM32MP15_HUK_BSEC_KEY_0, key++, &lock);
 	if (ret)
@@ -135,13 +141,12 @@ TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 	if (ret)
 		goto out;
 
-	ret = aes_gcm_encrypt_uid(p, len, hwkey->data, &len);
+	ret = aes_gcm_encrypt_uid((uint8_t *)otp_key, len, hwkey->data, &len);
 
 	if (len != HW_UNIQUE_KEY_LENGTH)
 		ret = TEE_ERROR_GENERIC;
 out:
-	memzero_explicit(p, HW_UNIQUE_KEY_LENGTH);
-	mempool_free(mempool_default, p);
+	memzero_explicit(otp_key, HW_UNIQUE_KEY_LENGTH);
 
 	if (ret)
 		return ret;
