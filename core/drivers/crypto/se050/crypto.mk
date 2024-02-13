ldts/optee_os

commit fb559031c25f2441af4fd2c5bb9d433fae4299fa

drivers: se050: allow configuring the Secure Element applet
Add CFG_CORE_SE05X_VER to allow configuring the desirable applet
version.
This enables making the driver compatible with newer elements.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/drivers/crypto/se050/crypto.mk
@@ -28,6 +28,8 @@ CFG_CORE_SE05X_INIT_NVM ?= n
 # Secure Element (SE) Non Volatile Memory object unless there is explicit
 # confirmation from the SE that the NVM object has been removed.
 CFG_CORE_SE05X_BLOCK_OBJ_DEL_ON_ERROR ?= n
+# Select the SE05X applet version for aligning the built-in features
++CFG_CORE_SE05X_VER ?= 03_XX
 
 # I2C bus baudrate (depends on SoC)
 CFG_CORE_SE05X_BAUDRATE ?= 3400000

commit dc21d1066588e756a85a04235e5938dc925940fb

[FIO fromtree] drivers: se050: fix default configuration for the SE applet
Invalid character was merged in the fixed commit.

Fixes: fb559031c25f ("drivers: se050: allow configuring the Secure Element applet")
Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

core/drivers/crypto/se050/crypto.mk
@@ -29,7 +29,7 @@ CFG_CORE_SE05X_INIT_NVM ?= n
 # confirmation from the SE that the NVM object has been removed.
 CFG_CORE_SE05X_BLOCK_OBJ_DEL_ON_ERROR ?= n
 # Select the SE05X applet version for aligning the built-in features
-+CFG_CORE_SE05X_VER ?= 03_XX
+CFG_CORE_SE05X_VER ?= 03_XX
 
 # I2C bus baudrate (depends on SoC)
 CFG_CORE_SE05X_BAUDRATE ?= 3400000